ea3dc81a6e5810bf1f789c13ee53b02f20140a11
diablo-shotgun <hassan.nadeem99@yahoo.com>
2021-03-28 22:48:01 +0500

    changes

---
+++ IBAPlatformerUnity/Assets/QuestionMenu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuestionMenu : MonoBehaviour
+{
+    public void YesGame(){
+        SceneManager.LoadScene(2);
+    }
+
+    public void NoGame(){
+        SceneManager.LoadScene(3);
+    }
+}
--- IBAPlatformerUnity/Assets/Scripts/Main Menu/MainMenu.cs
+++ IBAPlatformerUnity/Assets/Scripts/Main Menu/MainMenu.cs
@@ -12,4 +12,5 @@ public class MainMenu : MonoBehaviour
     public void QuitGame(){
         Application.Quit();
     }
+    
 }
