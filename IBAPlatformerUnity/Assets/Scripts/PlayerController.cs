c04d117537a27d4990bbf3173519a9059f3ae886
diablo-shotgun <hassan.nadeem99@yahoo.com>
2021-05-03 21:55:08 +0500

    MOBILE CONTROLS HERE

--- IBAPlatformerUnity/Assets/Scripts/PlayerController.cs
+++ IBAPlatformerUnity/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -17,7 +18,14 @@ public class PlayerController : MonoBehaviour
     public GameObject QuestionUI;
     public GameObject dimension;
     public GameObject dimensionTilemap;
+    public Button ButtonLeft;
+    public Button ButtonRight;
+    public Button ButtonDimension;
+    public Button ButtonJump;    
     public bool checkdim = false;
+
+    private bool rightMove = false;
+    private bool leftMove = false;
 
     private Animator anime;
 
@@ -36,6 +44,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
         if (Input.GetKeyDown(KeyCode.Space) && !jumpCheck)
         {
             anime.SetBool("jumpTrue", true);
@@ -84,6 +93,65 @@ public class PlayerController : MonoBehaviour
             r2D.velocity = new Vector2(0, r2D.velocity.y);
         }
 
+        */
+
+/*
+        if (Input.GetKeyDown(KeyCode.Space) && !jumpCheck)
+        {
+            anime.SetBool("jumpTrue", true);
+            r2D.velocity = new Vector2(r2D.velocity.x, jumpForce);
+            jumpCheck = true;
+            //anime.SetBool("jumpTrue", false);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //GetComponent<AudioSource>().Play();
+            if (jumpCheck) GetComponent<AudioSource>().Pause();
+            else GetComponent<AudioSource>().UnPause();
+            r2D.velocity = new Vector2(moveSpeed, r2D.velocity.y);
+            anime.SetBool("runTrue", true);
+            //GetComponent<AudioSource>().UnPause();
+            SPR.flipX = false;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //GetComponent<AudioSource>().Play();
+            if (jumpCheck) GetComponent<AudioSource>().Pause();
+            else GetComponent<AudioSource>().UnPause();
+            r2D.velocity = new Vector2(-moveSpeed, r2D.velocity.y);
+            anime.SetBool("runTrue", true);
+            // GetComponent<AudioSource>().UnPause();
+            SPR.flipX = true;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            if(!checkdim){
+                dimension.SetActive(true);
+                dimensionTilemap.SetActive(true);
+                checkdim = true;
+            }
+            else{
+                dimension.SetActive(false);
+                dimensionTilemap.SetActive(false);
+                checkdim = false;
+            }
+        }
+        else if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            GetComponent<AudioSource>().Pause();
+            anime.SetBool("runTrue", false);
+            jumpCheck = false;
+            r2D.velocity = new Vector2(0, r2D.velocity.y);
+        }
+        */
+
+        if(leftMove){
+            LeftMobile();
+        }
+        else if(rightMove){
+            RightMobile();
+        }
+
         if(checkdim){
             if(dimension.transform.localScale.y < 100){
                 dimension.transform.localScale += new Vector3(50f * Time.deltaTime,50f * Time.deltaTime, 0);
@@ -93,6 +161,57 @@ public class PlayerController : MonoBehaviour
             if(dimension.transform.localScale.y > 0){
                 dimension.transform.localScale = new Vector3(0, 0, 0);
             }
+        }
+    }
+
+    public void JumpMobile(){
+        anime.SetBool("jumpTrue", true);
+        r2D.velocity = new Vector2(r2D.velocity.x, jumpForce);
+        jumpCheck = true;
+        //anime.SetBool("jumpTrue", false);
+    }
+
+    public void LeftMobile(){
+        //GetComponent<AudioSource>().Play();
+        if (jumpCheck) GetComponent<AudioSource>().Pause();
+        else GetComponent<AudioSource>().UnPause();
+        r2D.velocity = new Vector2(-moveSpeed, r2D.velocity.y);
+        anime.SetBool("runTrue", true);
+        // GetComponent<AudioSource>().UnPause();
+        SPR.flipX = true;
+        leftMove = true;
+    }
+
+    public void RightMobile(){
+        //GetComponent<AudioSource>().Play();
+        if (jumpCheck) GetComponent<AudioSource>().Pause();
+        else GetComponent<AudioSource>().UnPause();
+        r2D.velocity = new Vector2(moveSpeed, r2D.velocity.y);
+        anime.SetBool("runTrue", true);
+        //GetComponent<AudioSource>().UnPause();
+        SPR.flipX = false;
+        rightMove = true;
+    }
+
+    public void CancelLeftRight(){
+        GetComponent<AudioSource>().Pause();
+        anime.SetBool("runTrue", false);
+        jumpCheck = false;
+        r2D.velocity = new Vector2(0, r2D.velocity.y);
+        leftMove = false;
+        rightMove = false;
+    }
+
+    public void DimensionMobile(){
+        if(!checkdim){
+            dimension.SetActive(true);
+            dimensionTilemap.SetActive(true);
+            checkdim = true;
+        }
+        else{
+            dimension.SetActive(false);
+            dimensionTilemap.SetActive(false);
+            checkdim = false;
         }
     }
 
